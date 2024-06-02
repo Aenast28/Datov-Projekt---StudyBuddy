6e3b1a5f0d6842dac7289949fc45229cef33c50f
Aenast28 <121029506+Aenast28@users.noreply.github.com>
2024-06-02 17:37:31 +0200

    Update superduper.py

--- superduper.py
+++ superduper.py
@@ -240,9 +240,10 @@ for message in st.session_state.messages:
 
 col1, col2 = st.columns([3, 2])
 with col2:
-    st.image("logo_fis.jpg", width=150)
+    st.image("logo_fis.jpg", width=150, use_column_width='always', caption='Logo', output_format='auto')
+    st.markdown("<style>div.stImage>img {float: right;}</style>", unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: center;'>Preview of the document</h1>", unsafe_allow_html=True)
-    pdf_container = st.container(height=350,border=True)
+    pdf_container = st.container(height=350, border=True)
     
 
 with col1:
