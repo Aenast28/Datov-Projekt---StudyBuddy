a33f3d41ef2f97c7aaad02872e41ca11139ab2e1
Aenast28 <121029506+Aenast28@users.noreply.github.com>
2024-06-02 11:10:29 +0200

    Update superduper.py

--- superduper.py
+++ superduper.py
@@ -221,7 +221,6 @@ if "mixtral_model" not in st.session_state:
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
- Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
