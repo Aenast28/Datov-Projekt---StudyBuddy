57b5db9709ac0c93c856d393b2bfa67a34fe3a57
Aenast28 <121029506+Aenast28@users.noreply.github.com>
2024-06-04 20:20:53 +0200

    Update superduper.py

--- superduper.py
+++ superduper.py
@@ -241,49 +241,38 @@ st.markdown(
 col1, col2 = st.columns([3, 2])
 
 with col2:
-    st.markdown(
-        "<img src='https://fis.vse.cz/wp-content/uploads/FIS_loga_FIS_CZ_2_FIS_CZ_kruhove_RGB_pro_obrazovku_FIS_2_logo_2_rgb_1772x1772_acf_cropped.jpg' width='170' style='float:right;'>",
-        unsafe_allow_html=True
-    )
+    st.markdown("<img src='https://fis.vse.cz/wp-content/uploads/FIS_loga_FIS_CZ_2_FIS_CZ_kruhove_RGB_pro_obrazovku_FIS_2_logo_2_rgb_1772x1772_acf_cropped.jpg' width='170' style='float:right;'>", unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: center;'>Preview of the document</h1>", unsafe_allow_html=True)
-    pdf_container = st.container()
+    pdf_container = st.container(height=650, border=True)
+
 
 with col1:
     st.markdown("<h1 class='vse-ai'>VŠE AI</h1>", unsafe_allow_html=True)
     st.markdown("<h2 class='study-buddy'>STUDY BUDDY</h2>", unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: left;'>Chat with the AI</h1>", unsafe_allow_html=True)
-    
-    chat_container = st.container()
-    
+    chat_container = st.container(height=650,border=True)
     with chat_container:
         if "messages" not in st.session_state:
             st.session_state.messages = []
-
-        for message in reversed(st.session_state.messages):
+        for message in st.session_state.messages:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
-
-        # Chat input at the bottom
         if prompt := st.chat_input("Jak mohu pomoci?"):
             # Add user message to chat history
             st.session_state.messages.append({"role": "user", "content": prompt})
-            
             # Display user message in chat message container
             with st.chat_message("user"):
                 st.markdown(prompt)
-            
-            # Generate response
-            response, name_file, chat_history = generate_response(prompt)
-            
             with st.chat_message("assistant"):
+                response, name_file, chat_history = generate_response(prompt)
                 st.markdown(response)
-            
             st.session_state.messages.append({"role": "assistant", "content": response})
 
-if "name_file" in locals() and name_file:
+if name_file:
         with open(name_file, "rb") as pdf_file:
             PDFbyte = pdf_file.read()
             with col2:
                 # Zobrazení PDF v kontejneru
                 with pdf_container:
                     pdf_viewer(PDFbyte)
+
