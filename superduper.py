dfde0dfc790b6812dd51101c0c8db7453a19a163
Aenast28 <filip.starek@seznam.cz>
2024-06-11 12:11:41 +0200

    A

--- superduper.py
+++ superduper.py
@@ -1,4 +1,4 @@
-__import__('pysqlite3')
+__import__("pysqlite3")
 import sys
 import json
 import base64
@@ -6,7 +6,8 @@ from pdf2image import convert_from_path
 from PIL import Image
 import streamlit as st
 import tempfile
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 import streamlit as st
 from typing import List, Dict
 from openai import OpenAI
@@ -32,21 +33,27 @@ st.set_page_config(
     page_title="VŠE AI Study Buddy",
     page_icon=":robot_face:",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="expanded",
 )
+
 
 # Cache models and embeddings to avoid reloading them
 @st.cache_resource
 def load_embeddings():
     return OpenAIEmbeddings()
 
+
 @st.cache_resource
 def load_vectorstore(_embeddings):
-    return Chroma(persist_directory='db', embedding_function=_embeddings)
+    return Chroma(persist_directory="db", embedding_function=_embeddings)
+
 
 @st.cache_resource
 def load_llm():
-    llm = DeepInfra(model_id="mistralai/Mixtral-8x22B-Instruct-v0.1", deepinfra_api_token="hIvZQRN11e1BLIeYghOFCahQYX18uXeY")
+    llm = DeepInfra(
+        model_id="mistralai/Mixtral-8x22B-Instruct-v0.1",
+        deepinfra_api_token="hIvZQRN11e1BLIeYghOFCahQYX18uXeY",
+    )
     llm.model_kwargs = {
         "temperature": 0.4,
         "repetition_penalty": 1.2,
@@ -54,6 +61,7 @@ def load_llm():
         "top_p": 0.90,
     }
     return llm
+
 
 @st.cache_resource
 def load_prompt():
@@ -70,32 +78,40 @@ def load_prompt():
         Context: {context}
         Question: {question}
         """,
-        input_variables=["context", "question"]
+        input_variables=["context", "question"],
     )
+
 
 @st.cache_resource
 def load_chat_chain(_llm, _prompt):
     return LLMChain(llm=_llm, prompt=_prompt)
 
+
 @st.cache_data
 def load_pdf_files(folder_path):
-    pdf_files = [f for f in os.listdir(folder_path) if f.endswith('.pdf')]
-    
+    pdf_files = [f for f in os.listdir(folder_path) if f.endswith(".pdf")]
+
     idents = []
     names = []
     years = []
     languages = []
-    
+
     for pdf_file in pdf_files:
-        filename_values = pdf_file.replace('.pdf', '').split('__')
-        ident, name, year, language = filename_values[0], filename_values[1], filename_values[2], filename_values[3]
-        
+        filename_values = pdf_file.replace(".pdf", "").split("__")
+        ident, name, year, language = (
+            filename_values[0],
+            filename_values[1],
+            filename_values[2],
+            filename_values[3],
+        )
+
         idents.append(ident)
         names.append(name)
         years.append(year)
         languages.append(language)
-    
+
     return idents, names, years, languages
+
 
 embeddings = load_embeddings()
 openai_lc_client5 = load_vectorstore(embeddings)
@@ -103,8 +119,10 @@ llm = load_llm()
 prompt = load_prompt()
 chat_chain = load_chat_chain(llm, prompt)
 idents, names, years, languages = load_pdf_files("docs")
+
+
 #############################
-#chatbot streamlit a funkce ##################
+# chatbot streamlit a funkce ##################
 #############################
 # Assume you have a similarity search function defined, which searches documents based on a query
 def similarity_search(query):
@@ -112,34 +130,36 @@ def similarity_search(query):
 
     # Add filters based on selections
     if selected_idents:
-        adjusted_filters.append({'Ident': {'$in': selected_idents}})
-    
+        adjusted_filters.append({"Ident": {"$in": selected_idents}})
+
     if selected_names:
-        adjusted_filters.append({'Name': {'$in': selected_names}})
-    
+        adjusted_filters.append({"Name": {"$in": selected_names}})
+
     if selected_years:
-        adjusted_filters.append({'Year': {'$in': selected_years}})
-    
+        adjusted_filters.append({"Year": {"$in": selected_years}})
+
     if selected_languages:
-        adjusted_filters.append({'Language': {'$in': selected_languages}})
-    
+        adjusted_filters.append({"Language": {"$in": selected_languages}})
+
     # Combine filters using '$and' to apply all conditions if there are multiple filters
     if len(adjusted_filters) > 1:
-        filter_query = {'$and': adjusted_filters}
+        filter_query = {"$and": adjusted_filters}
     elif len(adjusted_filters) == 1:
         filter_query = adjusted_filters[0]  # Use the single filter directly
     else:
         filter_query = {}  # No filters, match all documents
-    
+
     # Perform the similarity search with the adjusted filters
     # Assuming openai_lc_client5 is defined and configured correctly
-    return openai_lc_client5.similarity_search(query,k=2, filter=filter_query)
+    return openai_lc_client5.similarity_search(query, k=2, filter=filter_query)
 
 
 import re
 import os
 
-name_file=""
+name_file = ""
+
+
 def find_file_by_partial_name(directory, partial_name):
     pattern = re.compile(rf".*__{partial_name}__.*")
     for root, dirs, files in os.walk(directory):
@@ -148,6 +168,7 @@ def find_file_by_partial_name(directory, partial_name):
                 return os.path.join(root, file)
     return None
 
+
 def generate_response(query):
     global name_file  # Declare the global variable name_file
 
@@ -155,10 +176,10 @@ def generate_response(query):
     docs = similarity_search(query)
     top_documents = docs[:1]  # Select the top document
     top_documents1 = str(top_documents)  # Convert to string
-    
+
     # Clear the current string in name_file
     name_file = ""
-    
+
     # Search for the "Name" in the metadata of the documents
     match = re.search(r"metadata=\{.*'Name': '([^']*)'.*\}", top_documents1)
     if match:
@@ -167,7 +188,7 @@ def generate_response(query):
         file_path = find_file_by_partial_name(directory, partial_name)
         if file_path:
             name_file = file_path
-    
+
     # Create the context from the top documents
     document_context = "\n\n".join([doc.page_content for doc in top_documents])
     string_messages = [json.dumps(message) for message in st.session_state.messages]
@@ -187,13 +208,8 @@ def generate_response(query):
             "question": query,
         }
     )
-    
-        
 
-    
     return response["text"], name_file
-
-
 
 
 # Extract unique metadata values for filters
@@ -203,11 +219,11 @@ years = list(set(years))
 languages = list(set(languages))
 
 
-#os.environ["OPENAI_API_KEY"] ==st.secrets["OPENAI_API_KEY"]
+# os.environ["OPENAI_API_KEY"] ==st.secrets["OPENAI_API_KEY"]
 
-#embeddings = OpenAIEmbeddings()
-#persist_directory = 'db'
-#openai_lc_client5 = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+# embeddings = OpenAIEmbeddings()
+# persist_directory = 'db'
+# openai_lc_client5 = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
 
 st.markdown(
     """
@@ -221,7 +237,7 @@ st.markdown(
     
     </style>
     """,
-    unsafe_allow_html=True
+    unsafe_allow_html=True,
 )
 
 st.sidebar.title("Document Filters")
@@ -240,15 +256,14 @@ for name in names:
     st.sidebar.write(f"{name}")
 
 
-
 ###load model
-#llm = DeepInfra(model_id="mistralai/Mixtral-8x22B-Instruct-v0.1",deepinfra_api_token="hIvZQRN11e1BLIeYghOFCahQYX18uXeY")
-#llm.model_kwargs = {
+# llm = DeepInfra(model_id="mistralai/Mixtral-8x22B-Instruct-v0.1",deepinfra_api_token="hIvZQRN11e1BLIeYghOFCahQYX18uXeY")
+# llm.model_kwargs = {
 #    "temperature": 0.4,
 #    "repetition_penalty": 1.2,
 #    "max_new_tokens": 500,
 #    "top_p": 0.90,
-#}
+# }
 
 # Set a default model
 if "mixtral_model" not in st.session_state:
@@ -299,7 +314,7 @@ st.markdown(
     }
     </style>
     """,
-    unsafe_allow_html=True
+    unsafe_allow_html=True,
 )
 
 # JavaScript for column resizing
@@ -316,20 +331,28 @@ st.markdown(
     });
     </script>
     """,
-    unsafe_allow_html=True
+    unsafe_allow_html=True,
 )
 col1, col2 = st.columns([3, 2], gap="small")
 
 with col2:
-    st.markdown("<img class='logo' src='https://fis.vse.cz/wp-content/uploads/FIS_loga_FIS_CZ_2_FIS_CZ_kruhove_RGB_pro_obrazovku_FIS_2_logo_2_rgb_1772x1772_acf_cropped.jpg' width='150' style='float:right;'>", unsafe_allow_html=True)
-    st.markdown("<h1 class='preview-header' style='text-align: center;'>Preview of the document</h1>", unsafe_allow_html=True)
+    st.markdown(
+        "<img class='logo' src='https://fis.vse.cz/wp-content/uploads/FIS_loga_FIS_CZ_2_FIS_CZ_kruhove_RGB_pro_obrazovku_FIS_2_logo_2_rgb_1772x1772_acf_cropped.jpg' width='150' style='float:right;'>",
+        unsafe_allow_html=True,
+    )
+    st.markdown(
+        "<h1 class='preview-header' style='text-align: center;'>Preview of the document</h1>",
+        unsafe_allow_html=True,
+    )
     pdf_container = st.container(height=650, border=True)
 
 with col1:
     st.markdown("<h1 class='vse-ai'>VŠE AI</h1>", unsafe_allow_html=True)
     st.markdown("<h2 class='study-buddy'>STUDY BUDDY</h2>", unsafe_allow_html=True)
-    st.markdown("<h1 style='text-align: left;'>Chat with the AI</h1>", unsafe_allow_html=True)
-    
+    st.markdown(
+        "<h1 style='text-align: left;'>Chat with the AI</h1>", unsafe_allow_html=True
+    )
+
     # Container for the chat messages
     chat_container = st.container(height=650, border=True)
     with chat_container:
@@ -341,7 +364,7 @@ with col1:
 
     # Spacer to push the input to the bottom
     st.write('<div style="flex-grow: 1;"></div>', unsafe_allow_html=True)
-    
+
     # Chat input at the bottom of col1
     if prompt := st.chat_input("Ask your study buddy"):
         # Add user message to chat history
@@ -358,9 +381,9 @@ with col1:
 
 
 if name_file:
-        with open(name_file, "rb") as pdf_file:
-            PDFbyte = pdf_file.read()
-            with col2:
-                # Zobrazení PDF v kontejneru
-                with pdf_container:
-                    pdf_viewer(PDFbyte)
+    with open(name_file, "rb") as pdf_file:
+        PDFbyte = pdf_file.read()
+        with col2:
+            # Zobrazení PDF v kontejneru
+            with pdf_container:
+                pdf_viewer(PDFbyte)
